aa6dfc34aeab575a1117ff8ee7ab7cc11bc6e559
crewsycrews <danre1@bk.ru>
2018-01-17 16:30:58 +0300

    begging

--- generateTrianglePascal.php
+++ generateTrianglePascal.php
@@ -5,6 +5,12 @@ namespace App\Solution;
 function generate($rowNumber)
 {
     // BEGIN (write your solution here)
-    
+    for($i=0;$i<=$rowNumber;$i++)
+    {
+        for($j=0;$j<=$rowNumber*2+1;$j++)
+        {
+            
+        }
+    }
     // END
 }
