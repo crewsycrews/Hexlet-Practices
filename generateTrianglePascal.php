cad637ff3ea4778c13a560df5f1ac3252e091b30
crewsycrews <danre1@bk.ru>
2018-01-18 10:50:21 +0300

    generateTrianglePascal done

--- generateTrianglePascal.php
+++ generateTrianglePascal.php
@@ -5,34 +5,46 @@ namespace App\Solution;
 function generate($rowNumber)
 {
     // BEGIN (write your solution here)
-    $result=[[]];
+    $result = array_fill(0,$rowNumber+1,array_fill(0,$rowNumber*2+1,0));
     for ($i=0;$i<=$rowNumber;$i++)
     {
         for ($j=0;$j<=$rowNumber*2+1;$j++)
         {
-            if ($j==$rowNumber+1 && $i==0)
+            if ($j==$rowNumber && $i==0)
             {
-                $result[$i][$rowNumber+1]=1;
+                $result[$i][$rowNumber]=1;
             }
-            if ($i>=1)
+            elseif ($i>=1)
             {
-                
-                if (($result[$i-1][$j+1]==1 && $j==0) || ($result[$i-1][$j+1]==1 && empty($result[$i-1][$j-1])) )
-                {
-                    $result[$i][$j]==1;
+                if($j>0 && $j<$rowNumber*2)
+                {                    
+                    if($result[$i-1][$j-1]==1 && $j==$rowNumber*2+1)
+                    {
+                        $result[$i][$j]=1;
+                    }
+                    elseif ($result[$i-1][$j+1]!=0 || $result[$i-1][$j-1]!=0)
+                    {
+                        $result[$i][$j]=$result[$i-1][$j-1]+$result[$i-1][$j+1];
+                    }
                 }
-                elseif (($result[$i-1][$j-1]==1 && empty($result[$i-1][$j+1])) || ($result[$i-1][$j-1]==1 && $j==$rowNumber*2+1))
+                elseif($j==0)
                 {
-                    $result[$i][$j]==1;
+                    if($result[$i-1][$j+1]==1)
+                    {
+                        $result[$i][$j]=1;
+                    }
                 }
-                elseif ($result[$i-1][$j-1]!=0)
+                elseif($j==$rowNumber*2)
                 {
-                    $result[$i][$j]=$result[$i-1][$j-1]*$result[$i-1][$j+1];
+                    if ($result[$i-1][$j-1]==1) 
+                    {
+                        $result[$i][$j]=1;
+                    }
                 }
             }
         }
     }
-    return $result;
+    return array_values(array_filter($result[$rowNumber]));
     // END
 }
-print_r(generate(4));
+print_r(generate(1));
